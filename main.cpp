6365897a0082f48562e90e143f1de5d0ba8aa108
younes ferhat <y.ferhat@esi-sba.dz>
2025-01-06 21:16:05 +0100

    adding the crossover

--- main.cpp
+++ main.cpp
@@ -15,6 +15,21 @@ int main()
     up.resize(10,0.9);
 
     //algo.affichage(algo.genererPopulation(low,up));
+
+
+    /*
+    double crossoverRate = 0.8; // Exemple : Taux de crossover
+        for (int i = 0; i < pop_size; ++i) {
+            for (int j = 0; j < dim; ++j) {
+                double r = static_cast<double>(rand()) / RAND_MAX;
+                if (r < crossoverRate) {
+                    T[i][j] = mutant[i][j]; // Prendre le gène de mutant
+                } else {
+                    T[i][j] = P[i][j]; // Garder le gène de P
+                }
+            }
+        }
+    */
     return 0;
 
 
