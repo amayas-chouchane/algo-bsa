911cbf8128e474d2c82f7975b5ed0a11b374f9b4
younes ferhat <y.ferhat@esi-sba.dz>
2025-01-06 18:23:12 +0100

    create solution class

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 
 #include <iostream>
-#include "algorithme.h"
 
 #include <vector>
 
@@ -10,12 +9,12 @@ using namespace std;
 int main()
 {
     cout << "Hello world!" << endl;
-    algorithme algo {10,10};
+   // algorithme algo {10,10};
     vector <double> low, up ;
     low.resize(10,0.1);
     up.resize(10,0.9);
 
-    algo.affichage(algo.genererPopulation(low,up));
+    //algo.affichage(algo.genererPopulation(low,up));
     return 0;
 
 
---
+++ solution.h
@@ -0,0 +1,45 @@
+#ifndef SOLUTION_H_INCLUDED
+#define SOLUTION_H_INCLUDED
+
+#include <iostream>
+#include <vector>
+#include <math.h>
+#include <cstdlib>
+#include <limits>
+
+using namespace std;
+
+#define PI 3.14159265
+
+class Solution
+{
+public:
+    // Constructor with dimensions and function index (for example: Rosenbrock, Ackley, etc.)
+    Solution(int dim, int func_id);
+
+    // Fitness calculation
+     double getFitness()const ;                     // Get fitness value
+    void random(double lower, double upper); // Generate random solution within bounds
+
+    // Friend function for displaying Solution
+    friend ostream& operator<<(ostream& os, const Solution& sol);
+
+    // Public properties
+    vector<double> tabLine; // Representation of variables
+    int functionIndex;      // Type of the objective function (1=Rosenbrock, 2=Rastrigin, etc.)
+
+private:
+    double currentFitness; // Current fitness value
+    int dimension;         // Dimension of the solution
+
+    // Methods to evaluate specific objective functions
+    double rosenbrock(const vector<double>& solution);
+    double rastrigin(const vector<double>& solution);
+    double ackley(const vector<double>& solution);
+    double schwefel(const vector<double>& solution);
+
+    // Function to calculate fitness based on selected objective function
+ void fitness(const vector<double>& solution);
+};
+
+#endif // SOLUTION_H_INCLUDED
